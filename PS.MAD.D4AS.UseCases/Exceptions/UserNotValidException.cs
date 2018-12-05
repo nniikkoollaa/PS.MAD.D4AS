921f5321d8e1013a9cd561dc65461392e429cf3c
Nikola Milanovic <nniikkoollaa@gmail.com>
2018-12-05 16:42:56 +0100

    SubmitNewTicket updated with User validation

---
+++ PS.MAD.D4AS.UseCases/Exceptions/UserNotValidException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace PS.MAD.D4AS.UseCases.Exceptions
+{
+    public class UserNotValidException : Exception
+    {
+        public UserNotValidException()
+        {
+        }
+
+        public UserNotValidException(string message) : base(message)
+        {
+        }
+
+        public UserNotValidException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected UserNotValidException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
--- PS.MAD.D4AS.UseCases/SubmitNewTicket.cs
+++ PS.MAD.D4AS.UseCases/SubmitNewTicket.cs
@@ -1,20 +1,39 @@
-﻿using System;
+﻿using PS.MAD.D4AS.UseCases.Exceptions;
+using System;
 
 namespace PS.MAD.D4AS.UseCases
 {
     public class SubmitNewTicket
     {
         private readonly Contracts.ITicketRepository _repository;
+        private readonly Random _random;
 
         public SubmitNewTicket(Contracts.ITicketRepository repository)
         {
             this._repository = repository;
+            _random = new Random();
         }
 
         public Guid ForUser(Entities.User user, Entities.Ticket ticket)
         {
-            // validate if user has a valid insurance policy
-            return this._repository.Create(ticket);
+            // 1. Validate
+            this.Validate(user);
+
+            // 2. Save
+            var ticketId = this._repository.Create(ticket);
+
+            return ticketId;
+        }
+
+        private void Validate(Entities.User user)
+        {
+            System.Threading.Thread.Sleep(_random.Next(800, 1200));
+            var isValid = true;
+
+            if (!isValid)
+            {
+                throw new UserNotValidException();
+            }
         }
     }
 }
